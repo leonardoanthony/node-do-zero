2b6d760abbda318c2769460d54f37f6ce1dd2c38
leonardoanthony <leonardoanthony.dev@gmail.com>
2024-06-21 16:34:41 -0300

    API Node com Postgress

---
+++ server.js
@@ -0,0 +1,48 @@
+import {fastify} from "fastify";
+import { DatabaseMemory } from "./database-memory.js";
+import { DatabasePostgres } from "./database-postgres.js";
+
+const server = fastify();
+// const database = new DatabaseMemory();
+const database = new DatabasePostgres();
+
+server.post('/videos', async (request, reply) => {
+
+    const {title, description, duration} = request.body;
+
+    await database.create({
+        title,
+        description,
+        duration,
+    })
+
+    return reply.status(201).send();
+});
+
+server.get('/videos', async (request, reply) => {
+    const {search} = request.query;
+
+    const videos = await database.list(search);
+    return videos;
+});
+
+server.put('/videos/:id', async (request, reply) => {
+    const {id} = request.params;
+    const {title, description, duration} = request.body;
+
+    await database.update(id, {
+        title, description, duration
+    });
+
+    return reply.status(204).send();
+});
+
+server.delete('/videos/:id', async (request, reply) => {
+    const {id} = request.params;
+
+    await database.delete(id);
+
+    return reply.status(204).send();
+});
+
+server.listen({port: process.env.PORT ?? 3333});
